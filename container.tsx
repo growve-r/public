b72b82a77c0fb621aff3bd874e68112c6962d64d
John Rasmussen <80776513+growve-r@users.noreply.github.com>
2023-11-27 09:55:46 -0600

    Update container.tsx

--- container.tsx
+++ container.tsx
@@ -57,39 +57,40 @@ function Container() {
 
     // Return the JSX code for the component
     return (
-        <div className="w-4/5 max-w-xl mx-auto my-8 p-8 border border-gray-300 rounded-lg shadow-lg">
-            <h1 className="text-3xl text-blue-600">Create/Edit Endpoint</h1>
-            <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
-                <label
-                    htmlFor="file"
-                    className="block text-lg font-bold mb-2"
-                >
-                    Upload a JSON file
-                </label>
-                <input
-                    id="file"
-                    name="file"
-                    type="file"
-                    accept=".json"
-                    {...register("file", validationSchema)}
-                    className="block w-full py-2 px-4 border border-gray-300 rounded-md outline-none focus:border-blue-600"
-                />
-                {errors.file && (
-                    <div className="text-sm text-red-600 mt-1">
-                        {errors.file.message}
-                    </div>
-                )}
-                <button
-                    type="submit"
-                    className="block w-full py-3 px-4 bg-blue-600 text-white font-bold rounded-md cursor-pointer mt-4"
-                >
-                    Upload
-                </button>
-            </form>
-            {jsonData && (
-                <General formValues={formValues} setFormValues={setFormValues} />
-            )}
-        </div>
+        <div className="w-4/5 max-w-xl mx-auto my-8 p-8 border border-gray-300 rounded-lg shadow-lg bg-white">
+  <h1 className="text-3xl text-green-600">Create/Edit Endpoint</h1>
+  <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+    <label
+      htmlFor="file"
+      className="block text-lg font-bold mb-2 text-gray-700"
+    >
+      Upload a JSON file
+    </label>
+    <input
+      id="file"
+      name="file"
+      type="file"
+      accept=".json"
+      {...register("file", validationSchema)}
+      className="block w-full py-2 px-4 border border-gray-300 rounded-md outline-none focus:border-green-600"
+    />
+    {errors.file && (
+      <div className="text-sm text-red-600 mt-1">
+        {errors.file.message}
+      </div>
+    )}
+    <button
+      type="submit"
+      className="block w-full py-3 px-4 bg-green-600 text-white font-bold rounded-md cursor-pointer mt-4"
+    >
+      Upload
+    </button>
+  </form>
+  {jsonData && (
+    <General formValues={formValues} setFormValues={setFormValues} />
+  )}
+</div>
+
     )
 }
 
